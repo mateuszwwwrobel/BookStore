a5b0ce2ebe4eeb31d464bafc54c9d0341a138f2f
mateuszwwwrobel <mateusz.wwwrobel@gmail.com>
2021-08-01 14:39:16 +0100

    Rename admin classes

--- core/admin.py
+++ core/admin.py
@@ -3,14 +3,15 @@ from core.models import Author, Book
 
 
 @admin.register(Author)
-class ExpenseAdmin(admin.ModelAdmin):
+class AuthorAdmin(admin.ModelAdmin):
     list_display = ('name', 'created_at', 'updated_at')
     list_per_page = 25
     list_display_links = ('name', )
 
 
 @admin.register(Book)
-class BudgetAdmin(admin.ModelAdmin):
+class BookAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'isbn')
     list_per_page = 25
     list_display_links = ('id', 'title')
+    ordering = ('created_at', )
